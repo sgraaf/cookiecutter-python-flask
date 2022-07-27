06417952282b0b9b14f509297d87ede8d925cebc
sgraaf <8904453+sgraaf@users.noreply.github.com>
2022-07-27 19:29:51 +0200

    ✨ Improve `foreign_key_reference_col`

--- {{cookiecutter.project_name}}/{{ cookiecutter.package_name }}/database/utils.py
+++ {{cookiecutter.project_name}}/{{ cookiecutter.package_name }}/database/utils.py
@@ -1,5 +1,5 @@
 """Database utility functions."""
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 from sqlalchemy.orm import Mapped
 
@@ -7,18 +7,30 @@ from {{ cookiecutter.package_name }}.extensions import db
 
 
 def foreign_key_reference_col(
-    table: str,
+    column_or_model_or_table: Union[db.Column, db.Model, str],
     nullable: bool = False,
+    primary_key: bool = False,
     foreign_key_kwargs: Optional[Dict[str, Any]] = None,
-    column_kwargs: Optional[Dict[str, Any]] = None,
+    **kwargs,
 ) -> Mapped[int]:
     """Adds a foreign key reference column."""
     foreign_key_kwargs = foreign_key_kwargs or {}
-    column_kwargs = column_kwargs or {}
+
+    if isinstance(column_or_model_or_table, db.Column):
+        fk_column = column_or_model_or_table
+    elif isinstance(column_or_model_or_table, db.Model):
+        fk_column = column_or_model_or_table.id
+    elif isinstance(column_or_model_or_table, str):
+        fk_column = f"{column_or_model_or_table}.id"
+    else:
+        raise TypeError(
+            "`column_or_model_or_table` must be either a `Column`, `Model`, or `str`."
+        )
 
     return db.Column(
         db.Integer,
-        db.ForeignKey(f"{table}.id", **foreign_key_kwargs),
+        db.ForeignKey(fk_column, **foreign_key_kwargs),
         nullable=nullable,
-        **column_kwargs,
+        primary_key=primary_key,
+        **kwargs,
     )
